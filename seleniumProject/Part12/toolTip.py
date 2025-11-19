6fdaef0acaf36fd6f0bed554e347e8b1bcede0bf
wahidrana2108 <wahidzamanrana@gmail.com>
2025-11-19 12:17:25 +0600

    Implementation ToolTip Access

--- seleniumProject/Part12/toolTip.py
+++ seleniumProject/Part12/toolTip.py
@@ -1,3 +1,5 @@
+from operator import contains
+
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver import ActionChains
@@ -7,12 +9,32 @@ driver = webdriver.Edge()
 driver.maximize_window()
 driver.implicitly_wait(5)
 
-driver.get("https://practice.expandtesting.com/tooltips")
+def tooltip_text():
+    driver.get("https://practice.expandtesting.com/tooltips")
+    time.sleep(3)
+    btns = driver.find_elements(By.CSS_SELECTOR, "[id*='btn']")
+    actions = ActionChains(driver)
+    for btn in btns:
+        print(btn.text)
+        actions.move_to_element(btn).perform()
+        time.sleep(3)
 
-btn_top = driver.find_element(By.ID, "btn1")
+def google_tooltip():
+    driver.get("https://www.google.com/")
+    time.sleep(3)
+    tip = driver.find_element(By.NAME, "q")
+    actions = ActionChains(driver)
+    actions.move_to_element(tip).perform()
+    time.sleep(3)
+    tip_text = tip.get_attribute("title")
+    print(tip_text)
+    if tip_text == "Search":
+        print("Test Passed")
+    else:
+        print(f"Test Failed. Showing: {tip_text}")
 
-actions = ActionChains(driver)
-actions.move_to_element(btn_top).perform()
+# tooltip_text()
+google_tooltip()
 
 time.sleep(5)
 driver.quit()
